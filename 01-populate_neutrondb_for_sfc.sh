7a07ff4041ba6f8f933a60679bfa7157edd5b0e4
Alioune <alioune.ba@orange.com>
2016-06-15 08:56:39 -0400

    modif

--- 01-populate_neutrondb_for_sfc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo neutron-db-manage --subproject networking-sfc check_migration
-sudo neutron-db-manage --subproject networking-sfc branches
-sudo neutron-db-manage --subproject networking-sfc history
-sudo neutron-db-manage --subproject networking-sfc upgrade head
--- 05-os_create_flow_classifier.sh
+++ 05-os_create_flow_classifier.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # Don't forget to create port for the classifier 
+#neutron flow-classifier-create \
+# --ethertype IPv4 \
+# --source-ip-prefix 55.55.55.2/32 \
+# --logical-source-port 758d4c01-18ca-483b-999b-6d6b79384013 \
+# --destination-ip-prefix 55.55.55.7/32 \
+# --protocol tcp \
+# --source-port 1:65000 \
+# --destination-port 1:65000 FC1
+
 neutron flow-classifier-create \
- --ethertype IPv4 \
  --source-ip-prefix 55.55.55.2/32 \
- --logical-source-port 758d4c01-18ca-483b-999b-6d6b79384013 \
  --destination-ip-prefix 55.55.55.7/32 \
- --protocol tcp \
- --source-port 1:65000 \
- --destination-port 1:65000 FC1
+ FC1
