ae18ad14e540d8a2f9d8811d8f039d190e5c9629
Alioune BA <alioune.ba@orange.com>
2016-10-07 12:32:30 +0000

    openstack networking sfc

---
+++ install-ovs-2.5.90.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/yyang13/ovs_nsh_patches/master/start-ovs-deb.sh 
+chmod 755 start-ovs-deb.sh 
+/bin/sh ./start-ovs-deb.sh
---
+++ set-env-ops.sh
@@ -0,0 +1,5 @@
+export OS_USERNAME=admin
+export OS_PASSWORD=admin
+export OS_REGION_NAME=RegionOne
+export OS_TENANT_NAME=admin
+export OS_AUTH_URL=http://192.168.56.15:5000/v2.0
